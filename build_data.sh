227e5e682a9d673c36dc424fcae2904892d59239
Chris Henk <cahenk95@gmail.com>
2016-09-11 15:39:00 -0500

    fixed typo in bash script

--- build_data.sh
+++ build_data.sh
@@ -2,11 +2,11 @@
 
 echo "This script will build the python-compatible data set by running pre-processing scripts and statistic scripts"
 
-rm parsed\ data/*               # Remove any existing files
+rm parsed\ data/*                   # Remove any existing files
 
-python3 convert_to_email_dict   # Parse raw emails into email object, dump binary to disk
-python3 build_table.py          # Filters out unneeded headers, convert to table (also dumps header frequencies)
-                                # Calculate Feature 1
-                                # Calculate Feature 2
-                                # Calculate Feature 3
-                                # Calculate Feature etc
+python3 convert_to_email_dict.py    # Parse raw emails into email object, dump binary to disk
+python3 build_table.py              # Filters out unneeded headers, convert to table (also dumps header frequencies)
+                                    # Calculate Feature 1
+                                    # Calculate Feature 2
+                                    # Calculate Feature 3
+                                    # Calculate Feature etc
